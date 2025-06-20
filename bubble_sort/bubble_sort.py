0f9d98234a9bf2df9ed1d0fe7350bb809a421c75
Audrey Ayieko <audrey.ayieko@strathmore.edu>
2025-06-20 14:29:22 +0300

    bubble_sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,11 +1,11 @@
-def bubble_sort(arr):
-    n = len(arr)
+def bubble_sort(array):
+    n = len(array)
     for i in range(n):
         swapped = False
         for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
                 swapped = True
         if not swapped:
             break
-        return arr
+        return array
