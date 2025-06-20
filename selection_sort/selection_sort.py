f9c067283446549dd6c855650c3e4924df72d709
Audrey Ayieko <audrey.ayieko@strathmore.edu>
2025-06-20 13:53:50 +0300

    selection_sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,11 @@
-def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr  # Correct: return AFTER all loops finish
+
+# Test the function (outside the bubble_sort function)
+sorted_list = bubble_sort([30, 87, 16, 25, 12, 42])
+print(sorted_list)
